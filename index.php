2b91dc5185ed2f69770691be4d860bc5794a358e
bscantor3 <bscantor3@gmail.com>
2021-09-21 21:19:18 -0500

    Avances en el landing page, imagen principal, y animaciones del navbar

--- index.php
+++ index.php
@@ -12,12 +12,12 @@
 </head>
 
 <body>
-  <header id="principal-header">
+  <header id="principal-header" class="shadow h-auto">
     <section id="top-header" class="row align-items-center px-5 py-3 justify-content-between shadow-sm position-relative">
       <div class="col-9 col-sm-3 order-1 order-sm-1">
         <button class="btn shadow-sm header-button-1">Iniciar Sesión</button>
       </div>
-      <div class="col-12 col-sm-6 order-0 order-sm-2">
+      <div class="col-12 col-sm-6 pb-3 pb-sm-0 order-0 order-sm-2">
         <a href="index.php" id="principal-logo" class="d-flex justify-content-center">
           <img src="presentation/img/logos/logo.svg" class="img-header" alt="logo">
         </a>
@@ -31,9 +31,9 @@
         <i class="fas fa-caret-down mb-2"></i>
       </div>
     </section>
-    <nav id="nav-container" class="navbar navbar-expand navbar-light bg-light shadow-sm">
+    <nav id="nav-container" class="navbar navbar-expand navbar-light bg-light shadow-sm animation-toggle-2">
       <div class="collapse navbar-collapse" id="navbarNav ">
-        <ul id="nav-items" class="navbar-nav d-flex justify-content-center w-100 pb-4">
+        <ul id="nav-items" class="navbar-nav d-flex justify-content-center w-100">
           <li class="nav-item">
             <a class="nav-link" href="#">Sofas <br><img src="presentation/img/icons/sofa.svg" class="furniture-icon" alt="icon_sofa"></a>
           </li>
@@ -65,7 +65,7 @@
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Otros
             </a>
-            <ul class="dropdown-menu bg-light px-2" aria-labelledby="navbarDropdown">
+            <ul class="dropdown-menu bg-light  px-2" aria-labelledby="navbarDropdown">
               <li><a class="dropdown-item" href="#"><img src="presentation/img/icons/litera.svg" class="furniture-icon me-1" alt="icon_futon"> Taburetes</a></li>
               <li><a class="dropdown-item" href="#"><img src="presentation/img/icons/bar.svg" class="furniture-icon me-1" alt="icon_futon"> Mini bares</a></li>
               <li><a class="dropdown-item" href="#"><img src="presentation/img/icons/mesilla-de-noche.svg" class="furniture-icon me-1" alt="icon_futon"> Mesas de noche</a></li>
@@ -79,12 +79,15 @@
     </nav>
   </header>
   <main id="principal-main">
+    <button id="btn-toggle" class="btn rounded"><i class="fas fa-angle-double-up"></i></button>
     <section id="main-img" class="d-flex align-items-end">
-      <h1 class="main-title w-100">¡La modestia y elegancia echa a la medida!</h1>
+      <h1 class="main-title w-100">¡La elegancia y modestia adaptada a la medida!</h1>
     </section>
+    <h3 class="main-subtitle">¿Te gusta lo que ves?</h3>
   </main>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  <script src="presentation/js/app.js"></script>
 </body>
 
 </html>
