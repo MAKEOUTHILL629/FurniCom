7471fcddeea21e29ab07e04b19ed8f15d1ad6e52
bscantor3 <bscantor3@gmail.com>
2021-09-27 02:47:40 -0500

    Creación de página shop products, fixes con navbar y asociados.

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
     <div class="position-absolute d-flex decorate-container justify-content-center mt-2 top-100 start-50 translate-middle">
       <i class="fas fa-caret-down mb-2"></i>
     </div>
-    <nav id="nav-container" class="navbar navbar-expand navbar-light bg-light shadow-sm animation-toggle-2">
+    <nav id="nav-container" class="navbar navbar-expand navbar-light bg-light shadow-sm animation-toggle-2" aria-label="header-nav">
       <div class="position-absolute d-flex decorate-container justify-content-center mt-1 top-0 start-50 translate-middle">
         <i class="fas fa-caret-down mb-2"></i>
       </div>
@@ -278,71 +278,72 @@
           </div>
         </div>
       </section>
+      <footer id="landing-footer" class="container-fluid py-3">
+        <div class="row h-100 justify-content-center align-items-center">
+          <div class="col-lg-4 col-md-6 order-3 order-lg-1 order-md-2">
+            <div class="row justify-content-around">
+              <div class="d-flex flex-column w-auto border-1 ps-4 mt-3">
+                <div class="d-flex align-items-center pb-2">
+                  <i class="footer-icon fas fa-phone-square"></i>
+                  <p class="d-inline-block m-0 text-white ps-3">+57 3218739029</p>
+                </div>
+                <div class="d-flex align-items-center pb-2">
+                  <i class="footer-icon fas fa-envelope-square"></i>
+                  <p class="d-inline-block m-0 text-white ps-3">servicioalcliente@furnicom.co</p>
+                </div>
+                <div class="d-flex align-items-center pb-2">
+                  <i class="footer-icon fas fa-map-marker-alt"></i>
+                  <p class="d-inline-block m-0 text-white ps-3">Ak 68 #No 23 – 15, Bogotá, Cundinamarca</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 order-1 col-md-12 order-lg-2 order-md-1">
+            <div class="row align-items-center">
+              <a href="index.php" id="principal-logo" class="h-100 d-flex justify-content-center">
+                <img src="presentation/img/logos/logo_w.svg" class="img-footer" alt="logo">
+              </a>
+              <div class="d-flex justify-content-center pt-2">
+                <p class="fs-5 text-white">FurniCom © 2021</p>
+              </div>
+              <div class="d-flex justify-content-center align-items-center">
+                <a href="" class="w-auto">
+                  <i class="footer-icon fab fa-facebook"></i>
+                </a>
+                <a href="" class="w-auto px-3">
+                  <i class="footer-icon fab fa-instagram"></i>
+                </a>
+                <a href="" class="w-auto">
+                  <i class="footer-icon fab fa-twitter"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div id="eslogan-section" class="col-lg-4 col-md-6 order-2 order-lg-3 order-md-3">
+            <div class="d-flex w-100 justify-content-center">
+              <div class="eslogan-cont">
+                <p class="text-white border-1 ps-3 py-3">Bienvenida la fabricación mobiliaria de hogar y las estancias de trabajo, por más de 40 años.</p>
+              </div>
+            </div>
+            <div class="d-flex w-100 justify-content-center mt-2">
+              <div class="d-flex align-items-center px-2">
+                <i class="footer-icon fas fa-box"></i>
+              </div>
+              <div class="d-flex align-items-center px-2">
+                <i class="footer-icon fas fa-dolly-flatbed"></i>
+              </div>
+              <div class="d-flex align-items-center px-2">
+                <i class="footer-icon fas fa-shipping-fast"></i>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        </div>
+      </footer>
     </article>
   </main>
-  <footer id="landing-footer" class="container-fluid py-3">
-    <div class="row h-100 justify-content-center align-items-center">
-      <div class="col-lg-4 col-md-6 order-3 order-lg-1 order-md-2">
-        <div class="row justify-content-around">
-          <div class="d-flex flex-column w-auto border-1 ps-4 mt-3">
-            <div class="d-flex align-items-center pb-2">
-              <i class="footer-icon fas fa-phone-square"></i>
-              <p class="d-inline-block m-0 text-white ps-3">+57 3218739029</p>
-            </div>
-            <div class="d-flex align-items-center pb-2">
-              <i class="footer-icon fas fa-envelope-square"></i>
-              <p class="d-inline-block m-0 text-white ps-3">servicioalcliente@furnicom.co</p>
-            </div>
-            <div class="d-flex align-items-center pb-2">
-              <i class="footer-icon fas fa-map-marker-alt"></i>
-              <p class="d-inline-block m-0 text-white ps-3">Ak 68 #No 23 – 15, Bogotá, Cundinamarca</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-4 order-1 col-md-12 order-lg-2 order-md-1">
-        <div class="row align-items-center">
-          <a href="index.php" id="principal-logo" class="h-100 d-flex justify-content-center">
-            <img src="presentation/img/logos/logo_w.svg" class="img-footer" alt="logo">
-          </a>
-          <div class="d-flex justify-content-center pt-2">
-            <p class="fs-5 text-white">FurniCom © 2021</p>
-          </div>
-          <div class="d-flex justify-content-center align-items-center">
-            <a href="" class="w-auto">
-              <i class="footer-icon fab fa-facebook"></i>
-            </a>
-            <a href="" class="w-auto px-3">
-              <i class="footer-icon fab fa-instagram"></i>
-            </a>
-            <a href="" class="w-auto">
-              <i class="footer-icon fab fa-twitter"></i>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div id="eslogan-section" class="col-lg-4 col-md-6 order-2 order-lg-3 order-md-3">
-        <div class="d-flex w-100 justify-content-center">
-          <div class="eslogan-cont">
-            <p class="text-white border-1 ps-3 py-3">Bienvenida la fabricación mobiliaria de hogar y las estancias de trabajo, por más de 40 años.</p>
-          </div>
-        </div>
-        <div class="d-flex w-100 justify-content-center mt-2">
-          <div class="d-flex align-items-center px-2">
-            <i class="footer-icon fas fa-box"></i>
-          </div>
-          <div class="d-flex align-items-center px-2">
-            <i class="footer-icon fas fa-dolly-flatbed"></i>
-          </div>
-          <div class="d-flex align-items-center px-2">
-            <i class="footer-icon fas fa-shipping-fast"></i>
-          </div>
-        </div>
 
-      </div>
-    </div>
-    </div>
-  </footer>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
   <script src="presentation/js/jquery.min.js"></script>
--- presentation/js/app.js
+++ presentation/js/app.js
@@ -1,34 +1,93 @@
 const btn = document.getElementById("btn-toggle");
 let nav = document.getElementById("nav-container");
+let shop_main = document.getElementById("shop-main");
+let navbar = document.getElementById("nav-items");
 
-btn.addEventListener("click",()=>{
+let val = false;
+
+btn.addEventListener("click", () => {
   nav.classList.toggle("animation-toggle-2");
   nav.classList.toggle("animation-toggle-1");
   btn.classList.toggle("animation-btn");
+
+  if (shop_main) {
+    shop_main.classList.toggle("custom-padding");
+  }
 });
 
+$(window).resize(function () {
+  if (window.screen.width < 1000 && !val) {
+    val = !val;
+    let dropdown = document.getElementById("dropdown-nav");
+    navbar.removeChild(dropdown);
+    navbar.innerHTML = navbar.innerHTML + `
+      <li class="nav-item sp-itm">
+        <a class="nav-link" href="#">Literas <br><img src="presentation/img/icons/litera.svg" class="furniture-icon me-1" alt="icon_litera"></a>
+      </li>
+      <li class="nav-item sp-itm">
+        <a class="nav-link" href="#">Mini bares <br><img src="presentation/img/icons/bar.svg" class="furniture-icon me-1" alt="icon_bar"></a>
+      </li>
+      <li class="nav-item sp-itm">
+        <a class="nav-link" style="width:130px" href="#">Mesas de noche <br><img src="presentation/img/icons/mesilla-de-noche.svg" class="furniture-icon me-1" alt="icon_mesa_de_noche"></a>
+      </li>
+      <li class="nav-item sp-itm">
+        <a class="nav-link" style="width:140px" href="#">Muebles de Televisor <br><img src="presentation/img/icons/televisor.svg" class="furniture-icon me-1" alt="icon_televisor"></a>
+      </li>
+      <li class="nav-item sp-itm">
+        <a class="nav-link" href="#">Cómodas <br><img src="presentation/img/icons/armario.svg" class="furniture-icon me-1" alt="icon_armario"></a>
+      </li>
+      <li class="nav-item sp-itm">
+        <a class="nav-link" href="#">Futones <br><img src="presentation/img/icons/futon.svg" class="furniture-icon me-1" alt="icon_futon"></a>
+      </li>
+    `;
+  } else if (window.screen.width >= 1000 && val) {
+    val = !val;
+    const items = document.querySelectorAll(".sp-itm");
+    console.log(items);
+
+    for(item of items){
+      item.parentNode.removeChild(item);
+    }
+
+    navbar.innerHTML = navbar.innerHTML + `
+    <li id="dropdown-nav"class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Otros
+      </a>
+      <ul class="dropdown-menu bg-light drop-cont px-2" style="z-index: 3;" aria-labelledby="navbarDropdown">
+        <li><a class="dropdown-item" href="#"><img src="presentation/img/icons/litera.svg" class="furniture-icon me-1" alt="icon_litera"> Literas</a></li>
+        <li><a class="dropdown-item" href="#"><img src="presentation/img/icons/bar.svg" class="furniture-icon me-1" alt="icon_bar"> Mini bares</a></li>
+        <li><a class="dropdown-item" href="#"><img src="presentation/img/icons/mesilla-de-noche.svg" class="furniture-icon me-1" alt="icon_mesa_de_noche"> Mesas de noche</a></li>
+        <li><a class="dropdown-item" href="#"><img src="presentation/img/icons/televisor.svg" class="furniture-icon me-1" alt="icon_televisor"> Muebles de Televisor</a></li>
+        <li><a class="dropdown-item" href="#"><img src="presentation/img/icons/comoda.svg" class="furniture-icon me-1" alt="icon_comoda"> Cómodas</a></li>
+        <li><a class="dropdown-item" href="#"><img src="presentation/img/icons/futon.svg" class="furniture-icon me-1" alt="icon_futon"> Futones </a></li>
+      </ul>
+    </li>
+    `;
+  }
+});
 
 
 var owl = $('.owl-carousel');
 owl.owlCarousel({
-  loop:true,
-  responsiveClass:true,
-  autoplay:true,
-  lazyLoad:true,
-  center:true,
-  stagePadding:0,
-  autoplayTimeout:4000,
-  autoplayHoverPause:true,
-  responsive:{
-      0:{
-          items:1,
-      },
-      600:{
-          items:2,
-      },
-      1000:{
-          items:3,
-          stagePadding: 100,
-      }
+  loop: true,
+  responsiveClass: true,
+  autoplay: true,
+  lazyLoad: true,
+  center: true,
+  stagePadding: 0,
+  autoplayTimeout: 4000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+      stagePadding: 100,
+    }
   }
 });
